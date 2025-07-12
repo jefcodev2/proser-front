d33bcfd05b86ea61535f3553113d31fce18bdc84
Jefferson Pinchao <japinchaoc@gmail.com>
2025-07-12 12:10:29 -0500

    feat: route dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { NopageComponent } from './features/nopage/nopage.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
+  {path: 'dashboard', component: DashboardComponent},
+
   { path: 'login', component: LoginComponent },
-  { path: '**', component: NopageComponent } // Wildcard route for 404
+  { path: '**', component: NopageComponent } 
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
